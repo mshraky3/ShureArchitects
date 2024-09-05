147c02777fe36ba738db4e65a131bd23af72dafe
mshraky3 <93828510+mshraky3@users.noreply.github.com>
2024-09-06 01:23:56 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,23 +1,35 @@
 import express from "express";
 import bodyParser from "body-parser";
-
-
-
-
+import pg  from "pg"
 const app = express();
-
-
 
 app.use('/static', express.static('public'))
 
+const db  = new pg.Client({
+    password :"gdFRLYxirPld1F0MrJ1rsK6LVlDDvFjj" ,
+    host:"dpg-crd1mqg8fa8c73bg324g-a", 
+    database :"users_x5qf",
+    user : "users_x5qf_user",
+    port : 5432
+  })
+  
 
-app.get("/" , (req, res)=>{
-    res.render("home.ejs")
+db.connect()
+
+
+async function get_data (){
+    const data =  await db.query("SELECT * FROM users"); 
+ }
+ 
+
+
+app.get("/" , async (req, res)=>{
+    const data =  await db.query("SELECT * FROM users");
+    res.send(data)
 })
 
 app.get("/login", (req , res)=>{
     res.render("login.ejs")
-
 })
 
 app.get("/register", (req , res)=>{
@@ -27,7 +39,6 @@ app.get("/register", (req , res)=>{
 app.get("/uers-profile.ejs" , (req, res)=>{
     res.render("uers-profile.ejs")
 })
-
 
 
 
@@ -45,13 +56,25 @@ app.get("/list" , (req,res)=>{
 })
 
 
+app.get("/profile" , (req,res)=>{
+    res.render("profile.ejs")
+})
+
+
 app.post("/profile" , (req,res)=>{
     res.render("profile.ejs")
 })
 
 
-app.get("/profile" , (req,res)=>{
-    res.render("profile.ejs")
+app.post("/register" , (req,res)=>{
+
+    res.redirect("login.ejs")
+})
+
+
+app.post("/login" , (req,res)=>{
+
+    res.render("login.ejs")
 })
 
 
